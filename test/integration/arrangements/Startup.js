e578ec384bd6a64183ea9471ffc75ab26c3ed4d7
salvatorelaspata <salvatore.la.spata@gmail.com>
2022-04-12 10:49:46 +0000

    Deploying to gh-pages from @ CUBE-CONSULTANTS/sites@b005bdfe3a7ae37013dc328e16159ecd4b881208 🚀

---
+++ test/integration/arrangements/Startup.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/test/Opa5"],function(t){"use strict";return t.extend("it.cubeconsultants.site.test.integration.arrangements.Startup",{iStartMyApp:function(){this.iStartMyUIComponent({componentConfig:{name:"it.cubeconsultants.site",async:true,manifest:true}})}})});
---
+++ test/integration/opaTests.qunit.js
@@ -0,0 +1,1 @@
+QUnit.config.autostart=false;sap.ui.getCore().attachInit(function(){"use strict";sap.ui.require(["it/cubeconsultants/site/test/integration/AllJourneys"],function(){QUnit.start()})});
